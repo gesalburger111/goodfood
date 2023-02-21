8f945a4f4c7283fd25c3732d54cb9cc9be29af08
지민 <81950388+jjm6604@users.noreply.github.com>
2023-02-21 16:05:27 +0900

    Update index2.java

--- src/foodSelect/index2.java
+++ src/foodSelect/index2.java
@@ -2,7 +2,6 @@ package foodSelect;
 //// SSH 연결
 //import com.jcraft.jsch.JSch; 
 //import com.jcraft.jsch.Session;	
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -36,7 +35,7 @@ public class index2 {
     static String cate_name;
 
     public static void main_Frame(){
-        JFrame f = new JFrame("오늘 뭐 먹지?");
+        JFrame f = new JFrame("창원대 뭐 먹지?");
         f.setSize(500,200);
         f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -46,14 +45,14 @@ public class index2 {
         JButton china = new JButton("중식");
         JButton japan = new JButton("일식");
         JButton western = new JButton("양식");
-        JButton total = new JButton("전체 랜덤");					// total : 전체 랜덤
+        JButton total = new JButton("전체 랜덤");					// total : 전체랜덤
         
         
 
         Container contentPane = f.getContentPane();
         contentPane.setBackground(Color.pink);
 
-        contentPane.setLayout(new GridLayout(1,4));           //
+        contentPane.setLayout(new GridLayout(1,5));           //
 
         f.getContentPane().add(korea);
         f.getContentPane().add(china);
@@ -124,6 +123,7 @@ public class index2 {
     public static void sub_Frame(String cate){
         JFrame f2 = new JFrame("오늘의 " + cate + " 추천 메뉴는 ??");
         JPanel p2 = new JPanel();
+        
         f2.setSize(500,400);
         f2.setLocationRelativeTo(null);
         f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -132,41 +132,54 @@ public class index2 {
         Container contentPane = f2.getContentPane();
         contentPane.setBackground(Color.pink);
 
+        p2.setBackground(Color.pink);
         contentPane.setLayout(new BorderLayout());
-
+       
         if(cate == "한식"){
-            changed = "korea";
+            changed = "where id = \"korea\"";
             cate_name = "한식";
             select();
-            JLabel label = new JLabel(menu_list.get(random_data).getname()+" 입니다!");
+            JLabel label = new JLabel("<html><br><br><center>오늘의 추천 메뉴는 "+menu_list.get(random_data).getname()+" 입니다!"
+            		+ "<br><br><br>" + "창원대 근처의 " + menu_list.get(random_data).getname()+" 맛집 추천<br><br>"
+    				+ menu_list.get(random_data).getPlace()+"</center></html>");
             p2.add(label);
         }
         else if(cate == "중식"){
-            changed = "china";
+            changed = "where id = \"china\"";
             cate_name = "중식";
             select();
-            JLabel label = new JLabel(menu_list.get(random_data).getname()+" 입니다!");
+            JLabel label = new JLabel("<html><br><br><center>오늘의 추천 메뉴는 "+menu_list.get(random_data).getname()+" 입니다!"
+            		+ "<br><br><br>" + "창원대 근처의 " + menu_list.get(random_data).getname()+" 맛집 추천<br><br>"
+    				+ menu_list.get(random_data).getPlace()+"</center></html>");
             p2.add(label);
         }
         else if(cate == "일식"){
-            changed = "japan";
+            changed = "where id = \"japan\"";
             cate_name = "일식";
             select();
-            JLabel label = new JLabel(menu_list.get(random_data).getname()+" 입니다!");
+            JLabel label = new JLabel("<html><br><br><center>오늘의 추천 메뉴는 "+menu_list.get(random_data).getname()+" 입니다!"
+            		+ "<br><br><br>" + "창원대 근처의 " + menu_list.get(random_data).getname()+" 맛집 추천<br><br>"
+    				+ menu_list.get(random_data).getPlace()+"</center></html>");
             p2.add(label);
         }
         else if(cate == "양식"){
-            changed = "western";
+            changed = "where id = \"western\"";
             cate_name = "양식";
             select();
-            JLabel label = new JLabel(menu_list.get(random_data).getname()+" 입니다!");
+            JLabel label = new JLabel("<html><br><br><center>오늘의 추천 메뉴는 "+menu_list.get(random_data).getname()+" 입니다!"
+            		+ "<br><br><br>" + "창원대 근처의 " + menu_list.get(random_data).getname()+" 맛집 추천<br><br>"
+            				+ menu_list.get(random_data).getPlace()+"</center></html>");            
+           
+           
             p2.add(label);
         }
         else if(cate == "전체 랜덤"){													//
-            changed = "Total";
+            changed = "";
             cate_name = "전체 랜덤";
             select();
-            JLabel label = new JLabel(menu_list.get(random_data).getname()+" �엯�땲�떎!");
+            JLabel label = new JLabel("<html><br><br><center>오늘의 추천 메뉴는 "+menu_list.get(random_data).getname()+" 입니다!"
+            		+ "<br><br><br>" + "창원대 근처의 " + menu_list.get(random_data).getname()+" 맛집 추천<br><br>"
+    				+ menu_list.get(random_data).getPlace()+"</center></html>");
             p2.add(label);
         }
         
@@ -194,12 +207,13 @@ public class index2 {
             try {
                 Statement st = conn.createStatement();
                 ResultSet rs = null;
-                String sql = "select name from " + changed;
+                String sql = "select name, place from total " + changed ;
                 rs = st.executeQuery(sql);
 
                 while (rs.next()) {
                     UserBean bean = new UserBean();
                     bean.setname(rs.getString("name"));
+                    bean.setPlace(rs.getString("place"));
                     menu_list.add(bean);
                 }
 
