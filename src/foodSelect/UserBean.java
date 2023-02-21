7507c6795c1113aa389ec1bc1420703a443f8bf2
지민 <81950388+jjm6604@users.noreply.github.com>
2023-02-21 16:06:43 +0900

    Update UserBean.java

--- src/foodSelect/UserBean.java
+++ src/foodSelect/UserBean.java
@@ -3,10 +3,18 @@ package foodSelect;
 
 public class UserBean {
     String name;
+    String place;
     public String getname(){
         return name;
     }
     public void setname(String name) {
         this.name = name;
     }
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+    
 }
